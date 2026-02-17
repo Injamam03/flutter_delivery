fd818cd46752e6e2cdeb3257c9bffa7368cc4c92
Injamam Ul Hoque sifat <injamamulh544@gmail.com>
2026-02-17 09:18:19 +0600

    some udate Splashscreen class
    Added introductory comment to the Splashscreen class.

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -2,8 +2,9 @@ import 'package:deliver_app/Home%20Screen/shared_prefe.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart' show Get;
 import 'package:get/get_navigation/get_navigation.dart';
-
 import 'AppRoute.dart' show AppRoute;
+
+// ================== this is my intro screen or splashScreen =====================================
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
